57df5cb73c1d160393909e64538eafb4bcdc91b9
Krystian Habaj <habajkrystian@gmail.com>
2018-02-05 15:50:41 +0100

    Add Reactstrap. Css modules disabled.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Alert } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World.
+        <Alert color="success">
+          Reactstrap test!
+        </Alert>
       </div>
     );
   }
